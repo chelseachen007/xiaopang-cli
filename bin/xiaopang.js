d46a44ae0354ec2917b3864264e17779a516d55c
chelseachen007 <aqwejie@qq.com>
2020-09-03 18:58:10 +0800

    feat: dsds

--- bin/xiaopang.js
+++ bin/xiaopang.js
@@ -14,3 +14,4 @@ program.command('init <name>').description('init project').action(require('../li
 program.command('refresh').description('refresh routers...').action(require('../lib/refresh'))
 
 program.parse(process.argv)
+
